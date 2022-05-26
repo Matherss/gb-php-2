14ee3070ef7abfb5f6d9e6421f9f84f5eb5c20e9
Matherss <mathers_beats@mail.ru>
2022-05-26 11:43:24 +0300

    validate name input

--- views/v_reg.php
+++ views/v_reg.php
@@ -1,4 +1,12 @@
+<style>
+    .error {
+        margin:10px 0px;
+        color:red;
+        display:none;
+    }
+</style>
 <form id="login_form" method="post">
+    <div class="error">Имя может содержать только буквы</div>
     <label for="name">Ваше имя</label>
     <input type="text" name="name"></input>
     <label for="login">Ваш логин</label>
@@ -7,3 +15,14 @@
     <input type="text" name="pass"></input>
     <input type="submit" value="Зарегистрироваться"></input>
 </form>
+<script>
+    const nameEl = document.querySelector('input[name="name"]');
+    nameEl.addEventListener('change',(e)=>{
+        console.log(e.target.value);
+        if(!/^[a-zA-Zа-яёА-ЯЁ]+$/gi.test(e.target.value)){
+            document.querySelector(".error").style.display = "block";
+        }else {
+            document.querySelector(".error").style.display = "none";
+        };
+    })
+</script>
